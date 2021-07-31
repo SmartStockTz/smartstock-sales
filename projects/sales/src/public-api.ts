0f97f27ac885f559ab9e5d7753db5dc80c4bc8e9
joshuamshana <mama27j@gmail.com>
2021-07-31 18:02:49 +0300

    export more service and models

--- projects/sales/src/public-api.ts
+++ projects/sales/src/public-api.ts
@@ -4,3 +4,12 @@
 
 export * from './sales.module';
 export * from './services/sales-navigation.service';
+export * from './services/sale.service';
+export * from './services/sale.service';
+export * from './services/order.service';
+
+export * from './models/sale.model';
+export * from './models/cart.model';
+export * from './models/cart-item.model';
+export * from './models/order.model';
+export * from './models/customer.model';
