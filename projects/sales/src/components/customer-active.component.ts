26cd5bad7ffc899b7222990bbeaf5e2e05328120
joshua <mama27j@gmail.com>
2021-09-28 17:23:54 +0200

    change verified to observable.

--- projects/sales/src/components/customer-active.component.ts
+++ projects/sales/src/components/customer-active.component.ts
@@ -1,12 +1,13 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {CustomerModel, LibUserModel, UserService} from '@smartstocktz/core-libs';
+import {of} from 'rxjs';
 
 @Component({
   selector: 'app-customer-active',
   template: `
     <div *ngIf="currentUser" style="display: inline-block">
-      <div style="background: green; border-radius: 20px; width: 10px; height: 10px;display: inline-block" *ngIf="verified===true"></div>
-      <div style="background: red; border-radius: 20px; width: 20px; height: 10px; display: inline-block" *ngIf="verified===false"></div>
+      <div style="background: green; border-radius: 20px; width: 10px; height: 10px;display: inline-block" *ngIf="verified | async"></div>
+      <div style="background: red; border-radius: 20px; width: 20px; height: 10px; display: inline-block" *ngIf="verified | async"></div>
     </div>
   `,
   styleUrls: []
@@ -14,7 +15,7 @@ import {CustomerModel, LibUserModel, UserService} from '@smartstocktz/core-libs'
 export class CustomerActiveComponent implements OnInit {
   @Input() customer: CustomerModel;
   currentUser: LibUserModel;
-  verified = true;
+  verified = of(true);
 
   constructor(private readonly userService: UserService) {
   }
@@ -23,14 +24,14 @@ export class CustomerActiveComponent implements OnInit {
     this.userService.currentUser().then(value => {
       if (value.id === 'smartstock-hq') {
         // @ts-ignore
-        this.verified = this.customer.verified;
+        this.verified = of(this.customer.verified);
       } else {
-        this.verified = true;
+        this.verified = of(true);
       }
       this.currentUser = value;
     }).catch(reason => {
       console.log(reason);
-      this.verified = true;
+      this.verified = of(true);
     });
   }
 }
